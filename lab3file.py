afdee589a46c0587275f7f75752d3475957079dd
frahmati1 <156142399+frahmati1@users.noreply.github.com>
2024-02-12 18:56:06 +0000

    Add files via upload

---
+++ lab3file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def write_name_to_file():
+    with open("Farnoosh.txt", "w") as file:
+        file.write("Farnoosh")
+
+if __name__ == "__main__":
+    write_name_to_file()
